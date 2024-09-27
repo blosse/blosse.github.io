fcc6bdc095a07dade38ff0fcba16141543f53584
Titus Blosse <titus.blosse@gmail.com>
2024-09-27 14:29:50 +0100

    Another test commit to see if I fixed it.

--- experience-item.js
+++ experience-item.js
@@ -53,7 +53,6 @@ class Experience {
       descriptionList.appendChild(bullet);
     });
     descriptionField.appendChild(descriptionList);
-    // REMOVE THIS COMMENT
 
     // Start and end date
     const dateField = document.createElement("div");
