e0c8e5dca94d74b0bb5d5873fb1bb087dc1269c6
Titus Blosse <titus.blosse@gmail.com>
2024-10-05 11:27:29 +0100

    Removed bullets from descriptions and fiddled with the padding.

--- experience-item.js
+++ experience-item.js
@@ -1,6 +1,6 @@
 // This file holds the class for an experience-item
 class Experience {
-  constructor(company, location, title, duration, description = []) {
+  constructor(company, location, title, duration, description) {
     this.company = company;
     this.location = location;
     this.title = title;
@@ -22,23 +22,20 @@ class Experience {
       <p1>${this.duration}</p1>
       `;
 
-    var description = document.createElement("ul");
-    description.classList.add("experience-description-list");
+    var descriptionPadding = document.createElement("div");
+    descriptionPadding.classList.add("experience-description-padding");
+    var description = document.createElement("div");
+    description.classList.add("experience-description");
+    description.textContent = this.description;
 
     // Add event listener to each experience-item
     experience.addEventListener("click", function () {
       description.classList.toggle("show");
     });
 
-    this.description.forEach((item) => {
-      var desc = document.createElement("li");
-      desc.classList.add("experience-description-item");
-      desc.textContent = item;
-      description.appendChild(desc);
-    });
-
+    descriptionPadding.appendChild(description);
     experiencePadding.appendChild(heading);
-    experiencePadding.appendChild(description);
+    experiencePadding.appendChild(descriptionPadding);
     experience.appendChild(experiencePadding);
 
     return experience;
@@ -52,22 +49,14 @@ const education = [
     "Gothenburg",
     "Master's Degree in High Performance Computer Systems",
     "September 2022 to June 2024",
-    [
-      "Master’s program with a focus on high performance computers systems where I took courses on Parallel Programming, Computer Graphics, Computer Architecture, Sustainable Computing and Real-time systems.",
-      "Master’s thesis on optimizing message distribution for Bluetooth mesh gateways with the purpose of improving throughput and energy efficiency.",
-      "The thesis revolved around designing and implementing a message distribution service for an embedded Linux micro-service system using the C programming language.",
-    ],
+    "Master’s program with a focus on high performance computers systems where I took courses on Parallel Programming, Computer Graphics, Computer Architecture, Sustainable Computing and Real-time systems among others.\n\nI wrote my Master’s thesis on optimizing message distribution for Bluetooth mesh gateways with the purpose of improving throughput towards integrations such as Apple HomeKit and Google Home and to improve the overall energy efficiency of the gateways.\n\nThe thesis was written at Plejd AB and revolved around designing and implementing a message distribution service for an embedded Linux  micro-service system. The service was implemented in C.",
   ),
   new Experience(
     "Chalmers University of Technology",
     "Gothenburg",
     "Bachelor's Degree in Computer Science",
     "September 2019 to June 2022",
-    [
-      "Bachelor’s program in the field of computer science and engineering. Wide selection of courses on Algorithms and Data Structures, Computer Architecture and Embedded Systems, Maths, Computer Networks, Object Oriented Programming among others.",
-      "Bacherlor’s thesis on exploring musical interpretations of images using AI.",
-      "Exposure to a wide variety of programming languages such as C, C++, Java, Haskell, Python, Matlab and Assembler among others.",
-    ],
+    "Bachelor’s program in the field of computer science and engineering. Wide selection of courses on Algorithms and Data Structures, Computer Architecture and Embedded Systems, Maths, Computer Networks, Object Oriented Programming among others. Bacherlor’s thesis on exploring musical interpretations of images using AI. Exposure to a wide variety of programming languages such as C, C++, Java, Haskell, Python, Matlab and Assembler among others.",
   ),
 ];
 
@@ -78,67 +67,42 @@ const work = [
     "Gothenburg",
     "Summer internship",
     "June to August, 2023 and 2024",
-    [
-      "Internship at the department for vehicle motion management at Volvo Trucks during the two summers of my masters.",
-      "Development of a user interface displayed to the driver during automatic coupling of truck and trailer.",
-      "The interface was developed as an Android application using Kotlin and Jetpack Compose.",
-      "Implementation of a load balancing algorithm and an accompanying graphical interface for prototyping purposes in Python.",
-      "Version control using Git and GitHub.",
-    ],
+    "Internship  at the department for vehicle motion management at Volvo Trucks during the two summers of my Master's. The first summer I was tasked with implementation of a load balancing algorithm and an accompanying graphical interface in Python. The second summer I was tasked with development of a prototype user interface that is to be displayed to the driver during automatic coupling of truck and trailer. The interface was developed as an Android application using Kotlin and Jetpack Compose.",
   ),
   new Experience(
     "SGS Studenbostäder",
     "Gothenburg",
     "Groundskeeper / Janitor",
     "June 2022 to September 2022",
-    [
-      "Maintaining the outdoor and indoor residential areas at SGS Student Housing in Göteborg.",
-      "Gardening and lawn care.",
-      "Janitorial duties.",
-    ],
+    "Maintainence of the outdoor and indoor residential areas at SGS Student Housing in Göteborg. The job involved gardening and lawn care as well as some janitorial duties.",
   ),
   new Experience(
     "Track Recycle",
     "Gothenburg",
     "Shop Assistant",
     "January 2022 to March 2022",
-    [
-      "Shop assistant at a small store selling second hand outdoor equipment.",
-      "Customer service.",
-    ],
+    "Shop assistant at a small store selling second hand outdoor equipment.",
   ),
   new Experience(
     "Chalmers Golf Club",
     "Gothenburg",
     "Greenkeeper",
     "May to August, 2019 and 2020",
-    [
-      "Maintaing the golf course at Chalmers Golf Club in Landvetter.",
-      "Lawn care and irrigation.",
-      "Teamwork and problem solving.",
-      "Handling machinery and vehicles such as lawn mowers, bunker-rakers and tractors.",
-    ],
+    "Maintaing the golf course at Chalmers Golf Club in Landvetter. The work involved lawn care, irrigation and handling machinery. The work took a lot of team work and problemsolving to keep the course in top condition throughout the season",
   ),
   new Experience(
     "Skistar",
     "Sälen",
     "Ski Lift Attendant, Snow Maker, Terrain Park Crew",
     "December 2015 to April 2019",
-    [
-      "Ski lift attendant helping skiers ride the ski lifts safely in all weather conditions.",
-      "Problem solving and teamwork as me and my colleagues worked around the clock in the pre-season to make snow across the resort.",
-      "Builing and maintaining the jumps and rails in the various parks in Sälen, one of Swedens most popular ski resorts.",
-    ],
+    "For four winter seasons I worked at one of Swedens most popular ski resorts. I started out as a ski lift attendant helping skiers ride the lifts safely in all weather conditions. I then moved on to work as a snow maker, working with the snow production in the pre-seasons. For my last two winters I worked in the terrain parks, builing and maintaining jumps and rails in the various parks in Sälen.",
   ),
   new Experience(
     "TeliaSonera",
     "Norrköping",
     "Technical Support",
     "January 2015 to October 2017",
-    [
-      "Technical support for small and medium sized businesses at TeliaSonera, one of Sweden’s major telecoms.",
-      "Valuable work where I learned alot, both from a technical point of view but also regarding how to meet and work with people.",
-    ],
+    "Here I worked as technical support for small and medium sized businesses at TeliaSonera, one of Sweden’s major telecoms. This was my first proper job and it was very valuable work where I learned alot, both from a technical point of view but also regarding how to meet and work with people.",
   ),
 ];
 
@@ -191,7 +155,7 @@ expandWorkButton.addEventListener("click", function () {
   } else {
     // Less
     console.log("Close this thing");
-    for (var i = workListArray.length - 1; i > 2; i--) {
+    for (var i = workListArray.length - 1; i > 1; i--) {
       workListArray[i].style.maxHeight = "0px";
       workListArray[i].style.opacity = "0";
     }
